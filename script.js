9f3cbe4bf10c17f9b803eca6f06b29e7d3bf5ecf
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-04-25 08:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// write js code here if required
+document.addEventListener("DOMContentLoaded", function() {
+
+    var timerElement = document.getElementById("timer");
+
+
+    function updateTimer() {
+   
+        var currentDate = new Date();
+
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth() + 1;
+        var year = currentDate.getFullYear();
+        var hours = currentDate.getHours();
+        var minutes = currentDate.getMinutes();
+        var seconds = currentDate.getSeconds();
+
+        // Format the date and time
+        var formattedDate = `${day}/${month}/${year}`;
+        var formattedTime = `${hours}:${minutes}:${seconds}`;
+
+        timerElement.textContent = formattedDate + ", " + formattedTime;
+    }
+
+    updateTimer();
+
+    setInterval(updateTimer, 1000);
+});
